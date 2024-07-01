01469791467b825cf3c2b0d846fec0fd4ac40716
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-01 10:26:24 +0000

    02 /02 Deklaration + Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,1 +1,24 @@
-console.log("Hallo");
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+/**console.log("Hallo");
+console.log(Hallo);*/
+
+/* Dekalaration + Wertzuweisung */
+
+
+let firstName;  // Deklaration eine Variable
+console.log(firstName);    //Ausgabe
+firstName = "Marius";  //Wertzuweisung | assigment
+console.log(firstName); // Ausgabe
+let familyName = "Simen Ngassam"; // Deklaration + Wertzuweisung
+console.log("Hallo, I am " + firstName +" "+ familyName); // Hallo , I am  Marius Simen Ngassam
+
