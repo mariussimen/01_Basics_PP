de63c33482c3fccf8c34c9dc6c7809dab87adbfe
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-01 10:51:22 +0000

    02/02 Wertzuweisung + Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -15,10 +15,39 @@ console.log(Hallo);*/
 /* Dekalaration + Wertzuweisung */
 
 
-let firstName;  // Deklaration eine Variable
+/**let firstName;  // Deklaration eine Variable
 console.log(firstName);    //Ausgabe
 firstName = "Marius";  //Wertzuweisung | assigment
 console.log(firstName); // Ausgabe
 let familyName = "Simen Ngassam"; // Deklaration + Wertzuweisung
-console.log("Hallo, I am " + firstName +" "+ familyName); // Hallo , I am  Marius Simen Ngassam
+console.log("Hallo, I am " + firstName +" "+ familyName); // Hallo , I am  Marius Simen Ngassam*/
+
+/*
+    Wertzuweisung II +  Datentypen
+    JS ist ein untypisierte Sprache
+ */
+
+let test;
+// test = "HI";
+// test = 2;
+// undefined
+console.log("Datentyp: " + typeof test);  // Ausgabe Datentyp
+console.log("Inhalt: "+ test);   // Ausgabe Inhalt
+console.log("---------------")
+
+test ="HI"; //String
+console.log("Datentyp: " + typeof test);  // Ausgabe Datentyp
+console.log("Inhalt: "+ test);   // Ausgabe Inhalt
+console.log("---------------");
+
+test =2; 
+console.log("Datentyp: " + typeof test);  // Ausgabe Datentyp
+console.log("Inhalt: "+ test);   // Ausgabe Inhalt
+console.log("---------------")
+
+test = true;
+console.log("Datentyp: " + typeof test);  // Ausgabe Datentyp
+console.log("Inhalt: "+ test);   // Ausgabe Inhalt
+console.log("---------------")
+
 
