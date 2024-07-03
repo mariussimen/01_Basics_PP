a2502f53f6c8041d0604b7e978b5ce8b8dd33c11
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-03 07:19:53 +0000

    Berechnung Alter Iteration 2

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -7,10 +7,19 @@ const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
 // Berechnung Alter
-let year = 2024;
+//let year = 2024;
+//ageJohn= year - birthYearJohn;
+//ageMark = year - birthYearMark;
+
+// Berechnung Alter Iteration 2
+
+let date = new Date();
+let year = date.getFullYear();
+console.log("Datum " + date);
+console.log("Jahr " + date.getFullYear());
+
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
-
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
