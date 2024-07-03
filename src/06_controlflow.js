49fa73ca4faf9f60115d8dde6f5efe72fe9c6bcd
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-03 09:00:08 +0000

    update06

---
+++ src/06_controlflow.js
@@ -0,0 +1,28 @@
+// Entscheidungsstrukturen | control flow
+
+// Deklaration + Assignment
+const ageJohn = 40;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+if (isJohnOlder){
+  console.log("John is Older ");
+}
