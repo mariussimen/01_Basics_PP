bef4fe87ad23a1cc40812eece296714e07116b6d
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-03 10:30:19 +0000

    06/02 Fallunterscheidung / SWITCH|CASE 1

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -12,11 +12,11 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
+/*console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 console.log("isJohnOlder: " + isJohnOlder);
 console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+console.log("------------------");*/
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -52,7 +52,7 @@ else
 // mit alternativen Fällen (älter, jünger, gleich alt)
 
 
-if (isJohnOlder)
+/*if (isJohnOlder)
 {
     console.log("John is Older ");
 }
@@ -63,4 +63,38 @@ else if (isJohnEqual)
 else
 {
      console.log("John is younger : ");
-}
+}*/
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+ const firstName = "Jane";
+ let job;
+
+//job = "driver";  // .. fährt TAXI! / UBER
+//job = "diver"; // .. taucht im Rhein! 
+//job = "artist"; // .. malt ein Bild!
+// job = "pilot"; // .. macht etwas anderes! --> default
+ //job = "teacher"; // .. unterrichtet!
+ //job = "instructor"; // .. unterrichtet!
+
+/*switch (job) {
+    case "driver": // Job == "driver"
+        console.log(firstName + " .. fährt TAXI!");
+        break;
+    case "diver": // Job == "diver"
+        console.log(firstName + " .. taucht im Rhein!");
+        break;
+    case "artist": // Job == "artist"
+        console.log(firstName + " .. malt ein Bild");
+        break;
+    case "teacher": // Job == "Unterrichtet"
+    case "instructor":// Job == "Unterrichtet"
+        console.log(firstName + " .. unterrichtet")
+        break;
+    //case "instructor": // Job == "unterrichtet"
+      //  console.log(firstName + " ..unterrichtet")
+      //  break;
+    default: // Wichtig
+        console.log(firstName + " .. macht etwas anderes!");
+        break;
+}*/
