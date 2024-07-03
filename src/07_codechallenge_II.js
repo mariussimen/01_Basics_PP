4e0792d293a80a19bd1c96a99debe6cc7e0e6664
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-03 19:11:58 +0000

    07_Switch case

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -18,29 +18,56 @@ Alter 13-17 // Cola
 
 const prompt = require('prompt-sync')({sigint: true});
 
- let Name, Alter ;
+let Name, Alter ;
 
 Name  = prompt("Geben Sie Ihren Namen ein!!: ");
 Alter = parseInt(prompt("Geben Sie Ihren Alter ein!!! "));
 
-while (Alter<0) {
-    Alter = parseInt(prompt("Geben Sie Ihren Alter ein: "));
-}
+// while (Alter<0) {
+//     Alter = parseInt(prompt("Geben Sie Ihren Alter ein: "));
+// }
 console.log("Datentyp: " + typeof Name);
 console.log("Datentyp: " + typeof Alter);
 
-if(Alter>=0 && Alter<=5)
-    {
-    console.log(Name + " " + "trinkt Milch!!");
-    }
-else if (Alter>=6 && Alter<=12){
-        console.log(Name +" " + "trinkt Saft!! ");
-    }
-else if( Alter>=13 && Alter<=17)
-    {
-        console.log(Name + " "+ "trinkt Cola!! ");
-    }
-else{
-    console.log(Name + " " + "trinkt wein" );
+// if(Alter>=0 && Alter<=5)
+//     {
+//     console.log(Name + " " + "trinkt Milch!!");
+//     }
+// else if (Alter>=6 && Alter<=12){
+//         console.log(Name +" " + "trinkt Saft!! ");
+//     }
+// else if( Alter>=13 && Alter<=17)
+//     {
+//         console.log(Name + " "+ "trinkt Cola!! ");
+//     }
+// else{
+//     console.log(Name + " " + "trinkt wein" );
+// }
+
+//  ++++++++    Switch case ++++++
+
+// Alter = Number(Alter>=0 && Alter<=5) //" trinkt Milch";
+// Alter = Number(Alter>=6 && Alter<=12) //" trinkt saft";
+// Alter = Number(Alter>=13 && Alter<=17) //" trinkt Cola";
+// Alter = Number(Alter>=18) //"trinkt Wein";
+switch (Alter) {
+    case (Alter>=0 && Alter<=5
+        
+    ):
+        console.log(Name + " "+ " trinkt Milch")
+        break;
+    case (Alter>=6 && Alter<=12):
+        console.log(Name + " " + " trinkt Saft");
+        break;
+    case (Alter>=13 && Alter<=17):
+        console.log(Name + " " + "trinkt Cola");
+        break;
+    case (Alter>=18):
+        console.log(Name + " "+ " trinkt Wein");
+        break;
+    default:
+        console.log("Never executes");
+        // Alter = parseInt(prompt("Geben Sie Ihren Alter ein!!! "));
+        break;
 }
          
