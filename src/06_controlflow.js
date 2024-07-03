af729f131598f20e75a59143a2ce703b6ea62cc4
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-03 17:34:57 +0000

    07_Getränke-Challenge

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -67,8 +67,8 @@ else
 
 /****** Fallunterscheidung / SWITCH|CASE 1 ******/
 
- const firstName = "Jane";
- let job;
+//  const firstName = "Jane";
+//  let job;
 
 //job = "driver";  // .. fährt TAXI! / UBER
 //job = "diver"; // .. taucht im Rhein! 
@@ -98,3 +98,19 @@ else
         console.log(firstName + " .. macht etwas anderes!");
         break;
 }*/
+/********  Fallunterscheidung / SWITCH|CASE 2 *******/
+
+const cond = true;
+const value = 2;
+
+switch (cond) {
+    case (value == 1):
+        console.log("Value hat den Wert 1.");
+        break;
+    case (value == 2):
+        console.log("Value hat den Wert 2.");
+        break;
+    default:
+        console.log("Value hat einen anderen Wert.");
+        break;
+}
---
+++ src/07_codechallenge_II.js
@@ -0,0 +1,46 @@
+/*********   CODE CHALLENGE 02 **********/
+
+/*
+Geben über eine Prompt-Anforderung einen Namen ein
+Geben über eine Prompt-Anforderung ein Alter ein
+
+Geben Sie in Abhängigkeit vom angegebenen Alter 
+folgende Getränkeauswahl in die Konsole aus :
+
+Alter 0-5 // Milch  --> age > 0 AND age < 6
+Alter 6-12 // Saft
+Alter 13-17 // Cola
+18 + // Wein
+
+-->zB: "Max trinkt Cola."
+
+/* Getränke-Challenge */
+
+const prompt = require('prompt-sync')({sigint: true});
+
+ let Name, Alter ;
+
+Name  = prompt("Geben Sie Ihren Namen ein!!: ");
+Alter = parseInt(prompt("Geben Sie Ihren Alter ein!!! "));
+
+while (Alter<0) {
+    Alter = parseInt(prompt("Geben Sie Ihren Alter ein: "));
+}
+console.log("Datentyp: " + typeof Name);
+console.log("Datentyp: " + typeof Alter);
+
+if(Alter>=0 && Alter<=5)
+    {
+    console.log(Name + " " + "trinkt Milch!!");
+    }
+else if (Alter>=6 && Alter<=12){
+        console.log(Name +" " + "trinkt Saft!! ");
+    }
+else if( Alter>=13 && Alter<=17)
+    {
+        console.log(Name + " "+ "trinkt Cola!! ");
+    }
+else{
+    console.log(Name + " " + "trinkt wein" );
+}
+         
