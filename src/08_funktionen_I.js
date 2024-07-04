045caa197933e28252c659625ae9c40a842da6c5
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-04 09:06:29 +0000

    08-02Funktionen 02b

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -6,12 +6,37 @@
 //  Funktionsaufruf | call
 // test();
 
-
 // Funktionrumpf | callee
 // Funktionsdeklaration
 function test()
 {
 
     console.log("Hallo Marius!");
+}
 
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// call
+// ausgabeNamen()
+
+// Funktion
+function ausgabeNamen() {
+    // interne Variable | what happens in VEGAS ....
+    let firstName = "Marius";
+
+    console.log("Hallo " + firstName + "!");
+}
+// console.log(firstname) Fehler : Scope!
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamen2("Arian");// Argument ---> Daten für Parameter
+ausgabeNamen2("Maxime");
+ausgabeNamen2("Saba"); 
+
+function ausgabeNamen2( firstName) {
+   
+    console.log("Hallo " + firstName + "!");
 }
