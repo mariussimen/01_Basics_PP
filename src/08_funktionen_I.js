2575ee12eb1996ac39f07ee698db3e1fd77673c7
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-04 10:39:21 +0000

    update

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -43,9 +43,48 @@ function ausgabeNamen2( firstName) {
 
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter / Argumente
-const prompt = require('prompt-sync')({sigint: true});
-ausgabeNamensParams(prompt("Vorname?:"),prompt("Nachname:? "));
+// const prompt = require('prompt-sync')({sigint: true});
+// ausgabeNamensParams(prompt("Vorname?:"),prompt("Nachname:? "));
 
 function ausgabeNamensParams(firstName, familyName) {
     console.log("Hallo " + firstName + " " + familyName + " !");
 }
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+// ausgabeNamenSRP("Max","Mütze");
+
+// ausgabeNamenSRP("Max", "Mütze");
+
+function ausgabeNamenSRP(firstName, familyName) {
+    //  1. Funktionalität string composing
+    const GAP= " ";
+    let oputputStr = "Hallo," + firstName + GAP + familyName + "!";
+
+    // 2. Funktionalität: String composing
+    console.log(oputputStr);    
+}
+
+/***** Funktionen 03b *****/
+
+ // 1. Funktionalität: string composing
+ output(getString("max","Mütze"));
+
+function getString(firstName, familyName) {
+    const GAP =" ";
+    let oputputStr = "Hallo," + firstName + GAP + familyName + "!";
+    return oputputStr; // return schickt die Daten an den call---
+    console.log("hi")// nach Return wird die Funktion abgebrochen
+}
+// 2. Funktionalität: string output
+// output("Hi!");
+// output(2);
+// output(true);
+
+
+function output(outputData) {
+    console.log(outputData);
+ }
