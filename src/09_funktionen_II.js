25c226ea84ce2cfb18fb1bc025b5764729002378
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-05 10:27:35 +0000

    09/06 /getCheck /get num

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -13,15 +13,71 @@
 
 
 */
-const ERROR_STR_DIV = "Teilen durch 0 nicht möglich";
+const prompt = require('prompt-sync')({sigint: true});
 
-// module: division a / b |  test:
-output(divide(4,2));
-output(divide(3,2));
-output(divide(3,-2));
-output(divide(0,2));
-output(divide(3,0));
-output(divide(0,0));
+const ERROR_STR_DIV = "Teilen durch 0 nicht möglich";
+const ERROR_STR_GEN = "Irgendwas ging schief!";
+
+startApp()
+  function startApp() {
+	output(calculator(getNummer(),getNummer2(),getOp()));
+  }
+
+  function getNummer() {
+	  return parseInt(prompt("Geben Sie bitte  einem Zahl1?:  "));
+  }
+function getNummer2() {
+	return parseInt(prompt("Geben Sie bitte  einem Zahl?:  "));
+}
+function getOp() {
+	return prompt("Op?: ");
+}
+
+// module: calculator | tests:
+agreement : "+","-","*",":","/"
+// output(calculator(3,2,"+"));
+// output(calculator(3,2,"-"));
+// output(calculator(3,2,"*"));
+// output(calculator(3,2,":"));
+// output(calculator(3,2,"/"));
+// output(calculator(3,0,"/"));
+// output(calculator(3,2,"#?!"));
+
+function calculator(a,b,op) {
+	
+
+switch (op) {
+	case "+": // Addition
+		return add(a,b);
+	
+	case "-": // Subtraktion
+
+		return subtract(a,b);
+
+	case "*": // Multiplikation
+		return multiply(a,b);
+		
+
+	case "/": // Divison
+	case ":":
+		return divide(a,b);
+		
+	default:
+		return ERROR_STR_GEN;
+		
+}
+
+}
+
+
+
+// // module: division a / b |  test:
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(0,2));
+// output(divide(3,0));
+// output(divide(0,0));
 function divide(a,b) {
 	if (b==0){
 	return ERROR_STR_DIV;
