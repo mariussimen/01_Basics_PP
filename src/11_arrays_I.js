c8a63f991789eda89da6a8d50324faec7f6d6877
mariussimen <156855144+mariussimen@users.noreply.github.com>
2024-07-08 10:53:40 +0000

    11/05 Update

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -12,6 +12,15 @@
  *   PRAXIS
  * 
  */
+/* 02b. For-Schleife für Array-Index (Iteration)*/
+
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) 
+// {
+// //    output(i); 
+//    output(arr[i]);// Mapping 
+// }
+
 
 
 /*** 01. Funktionalität mit Einzelparametern */
@@ -43,15 +52,15 @@ function output(outputStr) {
 
 /****** 02. Theorie: Array  */
 
-let arr;
-// arr = new Array(); //Komstruktor
-//  arr = []; // Literal
-arr =[2,12,5,2,3];
+// let arr;
+// // arr = new Array(); //Komstruktor
+// //  arr = []; // Literal
+// arr =[2,12,5,2,3];
 
-output(arr);
-output(arr.length);// Anzahl der Elemente
-output(arr[0]); // Index 0 (1. Pos. im Array)
-output(arr[arr.length-1]); // letzte Pos. / allgemein
+// output(arr);
+// output(arr.length);// Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[arr.length-1]); // letzte Pos. / allgemein
 
 
 /*** 01a. Funktionalität mit Array 1 */
@@ -60,17 +69,98 @@ output(arr[arr.length-1]); // letzte Pos. / allgemein
 // arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
 // --> Transformation semantisches Problem --> numerisches Problem
 // output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
-output(getSentence(["Ich","bin","die","coole","Maxine","Mützerich"]));
-function getSentence(arr) {
+// output(getSentence(["Ich","bin","die","coole","Maxine","Mützerich"]));
+// // function getSentence(arr) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =   arr[0] + GAP +
+//                 arr[1] + GAP +
+//                 arr[2] + GAP +
+//                 arr[3] + GAP +
+//                 arr[4] + GAP +
+//                 arr[5] + GAP +
+//                 PUNCT;
+
+//     return str;
+// }
+
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+
+//     Inkrement ( untere grenze --> obere Grenze)
+// for (let i= 10; i <=100; i+=5) {   // i --> Schleifen - index
+//    output(i);
+    
+// }
+
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i>0; i--) {
+//     output(i);
+    
+// }
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+output(getSentenceArr2([192,168,10,25]))
+function getSentenceArr2(arr) {
     const GAP = " ";
     const PUNCT = ".";
-    let str =   arr[0] + GAP +
-                arr[1] + GAP +
-                arr[2] + GAP +
-                arr[3] + GAP +
-                arr[4] + GAP +
-                arr[5] + GAP +
-                PUNCT;
+    let str = " ";
+//     // 1 Iteration: Struktur  Genau berzeugen
+//    for (let i = 0; i < arr.length; i++) 
+//     if (i != arr.length - 1 ){// wenn du noch nicht am Ende des Arrays bist .... 
+//         str += arr[i] + GAP; // entweder;
+//        }else {
+//         str += arr[i] + PUNCT; // oder ;
+//        }
+//     return str ;
 
-    return str;
+//    2 Variante: Struktur erzeugen + trimmen()
+ for (let i = 0; i < arr.length; i++) {
+
+       str += arr[i] + GAP;
+    
+ }
+//   return str.trim() + PUNCT;
+  // 3 Varianten: Ausnutzen  von generischen Funktionen.....
+  return arr.join(GAP) + PUNCT;
 }
+
+/********* Überlegungen - Transponierung **********/
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+
+// let a = 0; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//    a += 1; // a = a + 1
+//    output("inhalt von a: " + a);
+// }
+
+// 1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+// Solange die Variable existiert, bleibt dieser erhalten
+// hier: Verkettung eines Strings // Transponierung
+// */
+
+// let str = ""; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//    str += "Test" + " "; // a = a + 1
+//    output(str);
+// }
